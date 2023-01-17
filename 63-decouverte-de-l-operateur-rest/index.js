98c705609364c1e9940475d07f10e7b47f94a780
Emilien-Ad-10 <BeAd.Git.Hub@proton.me>
2023-01-17 14:16:37 +0100

    Ajout de la vidéo63

---
+++ 63-decouverte-de-l-operateur-rest/index.js
@@ -0,0 +1,20 @@
+//Utilisation de l'opérateur rest dans une fonction lorsque le nombre de paramètre est inconnu:
+//previous = précédent
+//current = courant / actuel
+//current Value = valeur courante / actuelle
+
+function somme (...parametres){
+    return parametres.reduce((previous, current) => {
+        return previous + current;
+    })
+}
+
+console.log(somme(1, 3, 5, 7, 9));
+//la console affiche: 25
+
+//Affecter des éléments d'un tableau à une variable:
+let array01 = ["HTML", 5, "CSS", 3, "JavaScript", "GIT", "GITHUB"];
+console.log(array01);
+
+let [a, b, c, d, ...array02] = array01;
+console.log(array02);
